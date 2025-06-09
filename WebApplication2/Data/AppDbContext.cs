e055a9c4c1411a38743f9c8fa4f8295244349c94
Mateusz Wróblewski <s28673@pjwstk.edu.pl>
2025-06-09 10:14:53 +0200

    przygotowanie

---
+++ WebApplication2/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
